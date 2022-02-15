28766950a4617eb4c73147b8092cee4d30bf75c2
RastislavBiba <83280807+RastislavBiba@users.noreply.github.com>
2022-02-15 22:31:26 +0100

    Project with Updated class main and added class Kniha where is script for adding book in library system

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Book {
+    private String Author;
+    private String title;
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/example/demo/UmbController.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UmbController {
+
+    public UmbController(List<Book> books) {
+        this.books = init();
+    }
+
+    private List<Book> books;
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks(@RequestParam(required = false) String bookAuthor){
+        List<Book> filteredBooks = new ArrayList<>();
+        for (Book book : books){
+            if (book.getAuthor().equals(bookAuthor)){
+                filteredBooks.add(book);
+            }
+        }
+        Book book = new Book();
+        return this.books;
+    }
+
+
+    private List<Book> init(){
+        List<Book> books = new ArrayList<>();
+        Book book1 = new Book();
+        book1.setAuthor("Arthur");
+        book1.setTitle("Study in Red");
+        books.add(book1);
+
+        Book book2 = new Book();
+        book2.setAuthor("Michal");
+        book2.setTitle("Pisem knihu");
+        books.add(book2);
+
+        return books;
+    }
+
+    @GetMapping("/api/book/{bookId}")
+    public Book getBook(@PathVariable Integer bookId){
+    return this.books.get(bookId);
+    }
+    @PostMapping("/api/books")
+    public Integer createBook(@RequestBody Book book){
+        this.books.add(book);
+        return this.books.size()-1;
+    }
+    @DeleteMapping("/api/book/{bookId}")
+    public void DeleteBook(@PathVariable Integer bookId){
+        this.books.remove(this.books.get(bookId));
+    }
+    @PutMapping("/api/book/{bookId}")
+    public void updateteBook(@PathVariable Integer bookId, @RequestBody Book book){
+        this.books.get(bookId).setTitle(book.getTitle());
+        this.books.get(bookId).setAuthor(book.getAuthor());
+    }
+}
